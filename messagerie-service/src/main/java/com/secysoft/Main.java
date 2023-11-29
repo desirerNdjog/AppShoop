fc0482f1b507cdcacdf166f9e287a3ef0594449f
Désiré Junior NDJOG <junior.ndjog@kimia-technologies.com>
2023-11-29 15:38:41 +0100

    initial

---
+++ messagerie-service/src/main/java/com/secysoft/Main.java
@@ -0,0 +1,13 @@
+package com.secysoft;
+
+/**
+ * @author Desire Junior NDJOG
+ * @version 1.0
+ * @project ${PROJECT_NAME}
+ * @since ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
